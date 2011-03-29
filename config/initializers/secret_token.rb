ca482653fb7b554bdaa6be12b391a917850488c7
Joon Lee <seouri@gmail.com>
2011-03-29 17:36:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geneset::Application.config.secret_token = '7a12f3e0207db7afebfc999a4023c4314ff7d6ece3f50499a94e3a54dfd969c0ba1bfdc1c09a4360ace47d13469c11f3ddaa582cbc8a23d2ce796ac7090ad00f'
